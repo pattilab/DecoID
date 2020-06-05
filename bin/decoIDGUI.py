7876728f709a23c8cf12ff6fa7a30de386b9ae3a
e-stan <estancl1234@gmail.com>
2020-06-05 10:50:24 -0500

    gui working

--- bin/decoIDGUI.py
+++ bin/decoIDGUI.py
@@ -6,9 +6,13 @@ if __name__ == '__main__':
     multiprocessing.freeze_support()
     multiprocessing.set_start_method("spawn")
     #windll.shcore.SetProcessDpiAwareness(1)
-    sys.path.append(os.path.join(os.path.dirname(__file__),"..","src/DecoID/"))
-
-    from src.DecoID.GUIHelper import *
+    sys.path.append(os.path.join(os.path.dirname(__file__),"..","src","DecoID/"))
+    import DecoID
+    import mzCloudPy
+    import customDBpy
+    import keys
+    import MS2Search
+    from GUIHelper import *
 
     app = decoIDSearch()
     app.mainloop()
