e22aaf9f625b0cae720c58e5dc3f919ca9f92ce7
e-stan <estancl1234@gmail.com>
2020-06-18 11:32:43 -0500

    added datafile and docs

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. DecoID documentation master file, created by
+   sphinx-quickstart on Thu Jun 18 11:21:45 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to DecoID's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/setup.py
+++ src/setup.py
@@ -3,13 +3,13 @@ from setuptools import setup,find_packages
 setup(
   name = 'DecoID',         # How you named your package folder (MyLib)
   packages = ["DecoID"],   # Chose the same as "name"
-  version = '0.2.1',      # Start with a small number and increase it with every change you make
+  version = '0.2.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Metabolomics software for database assisted deconvolution of MS/MS spectra',   # Give a short description about your library
   author = 'Ethan Stancliffe',                   # Type in your name
   author_email = 'estancl1234@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/e-stan/DecoID',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/e-stan/DecoID/archive/v0.2.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/e-stan/DecoID/archive/v0.2.2.tar.gz',    # I explain this later on
   keywords = ['Metabolomics', 'Deconvolution', 'MS/MS',"Metabolite ID"],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
@@ -21,7 +21,7 @@ setup(
           'grequests',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 2 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
