ddcf8f8706f872f0b7c3077f55e154fc43326c80
e-stan <estancl1234@gmail.com>
2020-06-18 11:51:20 -0500

    bug fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ author = 'Ethan Stancliffe'
 # The full version, including alpha/beta/rc tags
 release = 'v0.2.2'
 
-
+master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
