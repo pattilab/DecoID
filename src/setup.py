ad0fc0a5da740c56a09d999ba9317a9f4efe6d25
e-stan <estancl1234@gmail.com>
2020-06-19 09:54:07 -0500

    small change to setup

--- src/setup.py
+++ src/setup.py
@@ -22,7 +22,7 @@ setup(
           'lxml'
       ],
   classifiers=[
-    'Development Status :: 2 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
