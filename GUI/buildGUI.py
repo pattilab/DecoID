260ee42123b272bd6436e402e4bf7f2bc531317b
e-stan <estancl1234@gmail.com>
2020-10-23 15:50:48 -0500

    small change to GUI build script

--- GUI/buildGUI.py
+++ GUI/buildGUI.py
@@ -12,7 +12,7 @@ PyInstaller.__main__.run([
     "--hidden-import=sklearn.utils._cython_blas",
     "--hidden-import=DecoID.DecoID",
     "--additional-hooks-dir=hook-sklearn.linear_model.py",
-    '--add-binary=%s' % 'C:/Users/Ethan/AppData/Local/Programs/Python/Python37/Lib/site-packages/sklearn/.libs/vcomp140.dll;.', #need to change to local directory of the .dll file
+    #'--add-binary=%s' % 'C:/Users/Ethan/AppData/Local/Programs/Python/Python37/Lib/site-packages/sklearn/.libs/vcomp140.dll;.', #need to change to local directory of the .dll file
     '--add-binary=%s' % "../databases/HMDB_experimental.db;.",
     '--add-binary=%s' % "../databases/MoNA-export-LC-MS-MS_Spectra.db;.",
     '--add-binary=%s' % '../src/DecoID/mzCloudCompound2TreeLinkageautoprocessing.pkl;.',
