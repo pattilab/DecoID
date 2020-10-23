c68a34d6f103911397e0b41f1335ef073ebaa0d6
e-stan <estancl1234@gmail.com>
2020-10-23 15:54:08 -0500

    small change to GUI build script

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_packages
 setup(
   name = 'DecoID',         # How you named your package folder (MyLib)
   packages = ["DecoID"],   # Chose the same as "name"
-  version = '0.2.4',      # Start with a small number and increase it with every change you make
+  version = '0.2.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Metabolomics software for database assisted deconvolution of MS/MS spectra',   # Give a short description about your library
   author = 'Ethan Stancliffe',                   # Type in your name
