167afd2e6d2db46fbc9047bf669487b196ce2ec7
e-stan <estancl1234@gmail.com>
2021-03-17 11:34:16 -0500

    changed version on doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, Ethan Stancliffe'
 author = 'Ethan Stancliffe'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.2.6'
+release = 'v0.2.9'
 master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 
