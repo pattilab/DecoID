739455c282f5d999886822b1a2773d40ed722e9a
Ethan Stancliffe <estancl1234@gmail.com>
2024-01-24 12:47:54 -0600

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -3,13 +3,13 @@ from setuptools import setup,find_packages
 setup(
   name = 'DecoID',         # How you named your package folder (MyLib)
   packages = ["DecoID"],   # Chose the same as "name"
-  version = '0.3.3',      # Start with a small number and increase it with every change you make
+  version = '0.3.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Metabolomics software for database assisted deconvolution of MS/MS spectra',   # Give a short description about your library
   author = 'Ethan Stancliffe',                   # Type in your name
   author_email = 'estancl1234@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/e-stan/DecoID',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/e-stan/DecoID/archive/v0.3.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/e-stan/DecoID/archive/v0.3.4.tar.gz',    # I explain this later on
   keywords = ['Metabolomics', 'Deconvolution', 'MS/MS',"Metabolite ID"],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
